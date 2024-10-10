6cff11cdb39f4e46b75acb4039d4ca0e51c3a412
sweekar2999 <106826093+sweekar2999@users.noreply.github.com>
2024-10-10 15:05:55 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ import {
     HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = "AIzaSyAB_a4-2EFDbExzkjOww27Yxku067tRlN0";
+const apiKey = "";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
