8c732509c7eda515bf0d5b16fcbffb8ddb04e2ad
sweekar <sweekardhaigude103@gmail.com>
2024-10-13 12:21:06 +0530

    Upadated Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
   return (
     <div className='main'>
         <div className='nav'>
-            <p>Gemini</p>
+            <p>Galaxy</p>
             <img src={assets.user_icon} alt="" />
         </div>
         <div className='main-container'>
